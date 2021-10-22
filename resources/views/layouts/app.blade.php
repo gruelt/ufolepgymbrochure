e6ae8d05b1fdebf073aad0f7ad70db69c9928fc4
gruelt <gruelt@gmail.com>
2021-10-23 00:39:16 +0200

    bs vue + template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,56 +21,56 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+        <div>
+            <b-nav>
+                <b-nav-item href="https://www.ufolepgym.com/brochure.htm">Ufolep Brochure</b-nav-item>
+                <b-nav-item>FJEP Gymnastique</b-nav-item>
 
-                    </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                @guest
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                    <b-nav-item  right href="{{Route('login')}}">{{__('Login')}}</b-nav-item>
+                    @if (Route::has('register'))
+
+                        <b-nav-item right href="{{ route('register') }}">{{ __('Register') }}</b-nav-item>
+
+                    @endif
+                @else
+
+
+                    <b-nav-item-dropdown text="{{ Auth::user()->name }}" right>
+                        <b-dropdown-item href="{{ route('logout') }}">
+
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                {{ __('Logout') }}
+                            </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+
+                        </b-dropdown-item>
+
+                    </b-nav-item-dropdown>
+
+                @endguest
+
+
+
+            </b-nav>
+        </div>
+
+        <div class="header">
+            <div class="title">{{ config('app.name', 'Laravel') }}</div>
+            <div class="subtitle">Because Subtitle is everything</div>
+            <b-img src="https://ufolepbrochure.s3.eu-west-3.amazonaws.com/agres/anneaux.png" width="75" center alt="Responsive image"></b-img>
+
+        </div>
+
+
 
         <main class="py-4">
             @yield('content')
